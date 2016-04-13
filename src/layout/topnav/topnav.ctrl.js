ff992ee45cd7fe136e7a86846d37e26a7064a265
Chris Maier <cjmaier3@gmail.com>
2016-04-13 13:38:24 -0500

    unsuccessful login alert, notification service scaffolding

--- src/layout/topnav/topnav.ctrl.js
+++ src/layout/topnav/topnav.ctrl.js
@@ -3,22 +3,29 @@ import loginTemplate from './../login/login.tpl.html'
 
 export default function topNav() {
   
- 
-  
-  TopNavController.$inject = ['$scope', '$q', '$mdDialog', 'dataservice','authservice'];
+  TopNavController.$inject = ['$scope', '$q', '$mdDialog', '$interval', 'dataservice','authservice', 'notificationservice'];
   /* @ngInject */
-  function TopNavController($scope, $q, $mdDialog,dataservice,authservice) {
+  function TopNavController($scope, $q, $mdDialog, $interval, dataservice, authservice, notificationservice) {
     var vm = this;
     vm.isAuthenticated = authservice.isAuthenticated();
     vm.username = vm.isAuthenticated? authservice.getUser(): 'Guest';
+    var interval = 1000;
     activate();
   
     function activate() {
       var promises = [];
       return $q.all(promises).then(function () {
-        //do stuff
+        $interval(function () {
+          updateNotifications();
+        }, interval);
+        // vm.notifications = notificationservice.getNotifications();
       });
     }
+
+    var updateNotifications = function() {
+      vm.notifications = notificationservice.getNotifications();
+    }
+
     vm.goToFilter = function () {
       $state.go('filter');
     }
