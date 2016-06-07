538c70024a4fe33c29765f5d467432455729d1fc
MBlanchard90 <matthewablanchard@gmail.com>
2016-06-07 15:57:17 -0500

    Updated geospatial dataset endpoints

--- src/marker/marker.data.js
+++ src/marker/marker.data.js
@@ -4,50 +4,51 @@ export default function InitMarkerApi(dataservice, onInverterMessage, onInverter
   dataservice.addEndpoint('retrieving weather markers', 
     {
       method: 'get',
-      url: 'api/weather',
+      url: 'api/weatherNode',
     }
   );    
   
   dataservice.addEndpoint('retrieving daily weather data',
     {
       method: 'get',
-      url: 'api/weather'
+      url: 'api/weatherDaily'
     }
   );
     
   dataservice.addEndpoint('retrieving recent daily weather data',
     {
       method: 'get',
-      url: 'api/weather'
+      url: 'api/weatherDaily'
     }
   );
   
   dataservice.addEndpoint('retrieving inverter markers',
     { 
       method: 'get',
-      url: 'api/inverter/nodes'
+      url: 'api/inverterNode'
     }
   );
   
     dataservice.addEndpoint('retrieving recent inverter data',
     { 
       method: 'get',
-      url: 'api/inverter'
+      url: 'api/inverterNode'
     }
   );
   
   
-  var inverterSocket = dataservice.addSocket('inverter', 'api/inverter', onInverterMessage, onInverterOpen);
+  var inverterSocket = dataservice.addSocket('inverter', 'api/inverterNode/MOCK_WS_INVITE/connect', onInverterMessage, onInverterOpen);
+  //var inverterSocket = {};
     
   return {
     //weather
-    getWeatherMarkers: function(lat,lon) { return dataservice.request('retrieving weather markers', {},'/'+ lat + '/' + lon + '/nodes');},
-    getDailyWeatherData: function(lat,lon,time) { return dataservice.request('retrieving daily weather data', {},'/'+ lat + '/' + lon + '/' + time + '/daily');},
-    getRecentDailyWeatherData: function(lat,lon) { return dataservice.request('retrieving recent daily weather data', {},'/'+ lat + '/' + lon + '/daily');},
+    getWeatherMarkers: function(lat,lon) { return dataservice.request('retrieving weather markers');},
+    getDailyWeatherData: function(lat,lon,time) { return dataservice.request('retrieving daily weather data', {},'/'+ lat + '/' + lon + '/' + time + '/');},
+    getRecentDailyWeatherData: function(lat,lon) { return dataservice.request('retrieving recent daily weather data', {},'/'+ lat + '/' + lon + '/latest');},
     
     //inverter
     getInverterMarkers: function() { return dataservice.request('retrieving inverter markers');},
-    getRecentInverterData: function(lat,lon) { return dataservice.request('retrieving recent inverter data', {},'/'+ lat + '/' + lon);}, 
+    getRecentInverterData: function(lat,lon) { return dataservice.request('retrieving recent inverter data', {},'/'+ lat + '/' + lon + '/latest');}, 
     inverterSocket: inverterSocket
   }
 }  
